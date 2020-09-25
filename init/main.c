0b25fcb69b10d42e5e749ebe38872c5ae8cc0947
Joe <joe.makepeace01@gmail.com>
2020-09-25 14:45:20 +0100

    Added putc() with basic cursor incrementing, doesn't scroll

--- init/main.c
+++ init/main.c
@@ -3,11 +3,14 @@
 
 #include <asm/io.h>
 
-const int color = 0x07;
+const unsigned char color = 0x07;
 uint16_t *tty_buf = (uint16_t *) 0xB8000;
 
-const int WIDTH = 80;
-const int HEIGHT = 25;
+const unsigned int WIDTH = 80;
+const unsigned int HEIGHT = 25;
+
+unsigned int x = 0;
+unsigned int y = 0;
 
 // Disables the inbuilt VGA cursor
 void discur() {
@@ -17,20 +20,40 @@ void discur() {
 
 // Clears the screen
 void clrscr() {
-  for (int i=0; i<(WIDTH * HEIGHT); i++) {
+  for (unsigned int i=0; i<(WIDTH * HEIGHT); i++) {
     tty_buf[i] = 0;
   }
+}
+
+void init_tty() {
+  discur();
+  clrscr();
+}
+
+// Increments the cursor
+void inccur() {
+  if (++x == WIDTH) {
+    x = 0;
+    if (++y == HEIGHT) {
+      y = 0;
+    }
+  }
+}
+
+// Prints a character to the screen
+void putc(char c) {
+  tty_buf[y * WIDTH + x] = (uint16_t) c | (uint16_t) color << 8;
+  inccur();
 }
 
 // Prints text to the screen
 void printf(const char *s) {
   for (int i=0; *s != '\0'; s++, i++) {
-    tty_buf[i] = (uint16_t) *s | (uint16_t) color << 8;
+    putc(*s);
   }
 }
 
 void kmain() {
-  discur();
-  clrscr();
+  init_tty();
   printf("Hello, World!");
 }
