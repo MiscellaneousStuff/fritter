ca49d4b6279e9392a95c66ca7ee473e2ae986278
Joe <joe.makepeace01@gmail.com>
2020-09-27 05:01:58 +0100

    Moved CLI to its own file within /kernel/cli.c and /include/cli.h

--- init/main.c
+++ init/main.c
@@ -10,35 +10,13 @@
 #include "gdt.h"
 #include "idt.h"
 
+#include "timer.h"
+ 
 #include "keyboard.h"
 #include "mouse.h"
 
-#include "timer.h"
-#include "rtc.h"
+#include "cli.h"
 
-// 219 = block
-
-void gets(char *s) {
-  size_t n = 0;
-  for (;;) {
-    char c = keyboard_getchar();
-    if (c) {
-      if (c == '\b' && n > 0) {
-        s--;
-        n--;
-        putc('\b');
-      } else if (c == '\n') {
-        *s = '\0';
-        break;
-      } else {
-        *s++ = c;
-        n++;
-        putc(c);
-      }
-    }
-  }
-}
- 
 int kmain(multiboot_t *mb_info) {
   // Init Global/Interrupt Descriptor Tables
   init_gdt();
@@ -49,9 +27,6 @@ int kmain(multiboot_t *mb_info) {
 
   // Init screen
   init_tty();
-
-  // Welcome message
-  printf("Welcome to fritter!\n");
 
   // Enable Interrupts
   asm volatile("sti");
@@ -67,45 +42,8 @@ int kmain(multiboot_t *mb_info) {
   init_vesa(mb_info);
   init_gui();
 
-  // Print char from user
-  char cmd[100];
-
-  // CMD loop
-  for (;;) {
-    // Prompt
-    printf("> ");
-
-    // Get Command
-    gets(cmd);
-    printf("\n");
-
-    // Determine command
-    if (strncmp(cmd, "help", strlen(cmd)) == 0) {
-      printf("clear\t Clears the screen\n");
-      printf("help\t Prints this help information");
-    } else if (strncmp(cmd, "clear", strlen(cmd)) == 0) {
-      clrscr();
-    } else if (strncmp(cmd, "echo ", 5) == 0) {
-      printf(cmd+5);
-    } else if (strncmp(cmd, "time", 4) == 0) {
-      printf("%04d/%02d/%02d %02d:%02d:%02d", year, month, day, hour, minute, second);
-    } else if (strncmp(cmd, "mouse", 5) == 0) {
-      printf("mouse co-ords := X: %d Y: %d", mouse_x, mouse_y);
-    } else if (strncmp(cmd, "vbe", 3) == 0) {
-      printf("Bootloader:\t\t%s\n", mb_info->boot_loader_name);
-      printf("VBE Mode:\t\t%d\n", mb_info->vbe_mode);
-      printf("MEM Upper\t\t0x%x\n", mb_info->mem_upper);
-      printf("MEM Lower\t\t0x%x\n", mb_info->mem_lower);
-      printf("Boot Device\t\t%d\n", mb_info->boot_device);
-      printf("Module Count\t\t%d\n", mb_info->mods_count);
-      printf("CMD Line\t\t0x%x", mb_info->cmdline);
-    } else {
-      printf("%s is an unknown command. Try again.", cmd);
-    }
-
-    // Newline
-    printf("\n");
-  }
+  // Init CLI
+  init_cli();
 
   // Wait for timing events
   for (;;) {
---
+++ kernel/cli.c
@@ -0,0 +1,70 @@
+#include <stddef.h>
+
+#include <fritter/kernel.h>
+
+#include "rtc.h"
+
+#include "keyboard.h"
+#include "mouse.h"
+
+// ASCII 219 = block
+void gets(char *s) {
+  size_t n = 0;
+  for (;;) {
+    char c = keyboard_getchar();
+    if (c) {
+      if (c == '\b' && n > 0) {
+        s--;
+        n--;
+        putc('\b');
+      } else if (c == '\n') {
+        *s = '\0';
+        break;
+      } else {
+        *s++ = c;
+        n++;
+        putc(c);
+      }
+    }
+  }
+}
+
+void init_cli() {
+  // Welcome message
+  printf("Welcome to fritter!\n");
+  
+  // Print char from user
+  char cmd[100];
+
+  // CMD loop
+  for (;;) {
+    // Prompt
+    printf("> ");
+
+    // Get Command
+    gets(cmd);
+    printf("\n");
+
+    // Determine command
+    if (strncmp(cmd, "help", strlen(cmd)) == 0) {
+      printf("clear\t Clears the screen\n");
+      printf("echo\t Displays text to the screen\n");
+      printf("help\t Prints this help information\n");
+      printf("mouse\t Prints mouse information to the screen\n");
+      printf("time\t Displays current date and time (Non-DST aware)");
+    } else if (strncmp(cmd, "clear", strlen(cmd)) == 0) {
+      clrscr();
+    } else if (strncmp(cmd, "echo ", 5) == 0) {
+      printf(cmd+5);
+    } else if (strncmp(cmd, "time", 4) == 0) {
+      printf("%04d/%02d/%02d %02d:%02d:%02d", year, month, day, hour, minute, second);
+    } else if (strncmp(cmd, "mouse", 5) == 0) {
+      printf("mouse co-ords := X: %d Y: %d", mouse_x, mouse_y);
+    } else {
+      printf("%s is an unknown command. Try again.", cmd);
+    }
+
+    // Newline
+    printf("\n");
+  }
+}
